7949831b922f0dd3cd980c29d3e1dca51db21b14
triannasres <18220004@std.stei.itb.ac.id>
2022-11-09 16:22:40 +0700

    all done

--- appchapter5.py
+++ appchapter5.py
@@ -1,22 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,render_template
 
 app = Flask(__name__)
-# curl -i -X GET "http://127.0.0.1:5000/greet?fname=Jason&lname=Statham"
+# curl -i -X GET "http://127.0.0.1:5000/tstchapter5?fname=Jason&lname=Statham"
+
+@app.get("/")
+def template():
+    return  render_template( "index.html" )
 
 @app.get("/tstchapter5")
-def index ():
-    fname = request.args.get( "fname" )
-    lname = request.args.get( "lname" )
+def index():
+    fname = request.args.get("fname")
+    lname = request.args.get("lname")
     if not fname and not lname:
-        # If both first name and last name are missing, return an error
         return jsonify({ "status" : "error" })
     elif fname and not lname:
-        # If first name is present but last name is missing
         response = { "data" : f"Hello, {fname} !" }
     elif not fname and lname:
-        # If first name is missing but last name is present
         response = { "data" : f"Hello, Mr. {lname} !" }
     else :
-        # if none of the above is true, then both names must be present
         response = { "data" : f"Is your name {fname} {lname} ?" }
     return jsonify(response)
+
+if __name__ == "__main__" :
+    app.run()
--- appchapter6.py
+++ appchapter6.py
@@ -1,10 +1,11 @@
 from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
+
 @app.get("/")
 def index():
     return  render_template( "index.html" )
-@app.get("/search")
 
+@app.get("/search")
 def search():
     args = request.args.get( "q" )
     return redirect(f"https://google.com/search?q={args}")
